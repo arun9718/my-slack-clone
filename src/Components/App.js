82af3cfbe2739e1f0642f7cdeb0fc5f68ad52d0f
arun9718 <arunavasahachowdhury@gmail.com>
2020-09-14 22:03:04 +0530

    Cleaning repo and learning react

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SignIn } from './';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Hello World!</h1>
+          <SignIn />
+      </header>
+    </div>
+  );
+}
+
+export default App;
